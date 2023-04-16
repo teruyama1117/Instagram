2985c064fdd8ed5082fb2aec90f25ef3f1a8ef62
teruteru <teruterubouzu@techacademy.jp>
2023-04-16 17:18:24 +0900

    Initial Commit

---
+++ Instagram/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Instagram
+//
+//  Created by Teruo Yamamoro on 2023/04/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
